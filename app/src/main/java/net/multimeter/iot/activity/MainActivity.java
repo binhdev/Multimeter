0f3a8c0a9d1773bd8efeaf937ed01ae2c285ba89
Binh <ttbinh@hueic.edu.vn>
2018-11-27 06:22:59 +0700

    main layout

--- app/src/main/java/net/multimeter/iot/activity/MainActivity.java
+++ app/src/main/java/net/multimeter/iot/activity/MainActivity.java
@@ -1,13 +1,38 @@
 package net.multimeter.iot.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import net.multimeter.iot.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.btn_osc)
+    public ImageView btnOsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        ButterKnife.bind(this);
+    }
+
+    public void onClickHandle(View view) {
+        switch (view.getId()) {
+            case R.id.btn_osc:
+                Intent intent = new Intent(this, GraphActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
